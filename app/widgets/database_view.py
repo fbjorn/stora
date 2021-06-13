fa9717c08af890e1a0d7c96a8ee56dc1eb37fb1f
fbjorn <fbjorn@users.noreply.github.com>
2021-06-13 23:16:13 +0300

    Item preview dialog; Make it possible to remove documents

--- app/widgets/database_view.py
+++ app/widgets/database_view.py
@@ -1,29 +1,32 @@
+from functools import partial
+from typing import List, Optional
+
 from google.cloud.firestore_v1 import CollectionReference
 from PySide6.QtCore import Qt, Slot
-from PySide6.QtWidgets import QMenu, QTableWidgetItem, QTreeWidgetItem
+from PySide6.QtGui import QKeyEvent
+from PySide6.QtWidgets import QHeaderView, QMenu, QTableWidgetItem, QTreeWidgetItem
 
 from app.db import get_firestore_emulator_client
 from app.widgets.auto.database_view import DatabaseViewWidget
-
-
-class TableAction:
-    REMOVE_DOC = "Remove document"
+from app.widgets.dialogs.document_value import DocumentValueDialog
 
 
 class TableItem(QTableWidgetItem):
-    def __init__(self, db_id: str, *args):
+    def __init__(self, db_id: str, content: str, *args):
         super().__init__(*args)
         self.db_id = db_id
+        self.content = content
+        self.setText(str(self.content))
 
 
 class DatabaseView(DatabaseViewWidget):
     def __init__(self, db_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.table = self.w_collection_table.w_table
+        self.tree = self.w_collections_tree.w_tree
 
         self.db_name = db_name
         self.db = get_firestore_emulator_client(self.db_name)
-        self.row_to_db_id = {}
 
         self._init_ui()
         self._connect_slots()
@@ -35,32 +38,52 @@ class DatabaseView(DatabaseViewWidget):
         self.w_stack.setCurrentWidget(self.page_info)
 
         self.table.setContextMenuPolicy(Qt.CustomContextMenu)
-        # self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
     def _connect_slots(self):
         self.w_collections_tree.b_refresh.clicked.connect(self.refresh_collections_list)
-        self.w_collections_tree.w_tree.itemClicked.connect(
-            self._on_collection_double_clicked
+        self.tree.itemClicked.connect(self._on_collection_double_clicked)
+        self.table.customContextMenuRequested.connect(self.create_table_context_menu)
+        self.table.keyPressEvent = self._on_table_item_key_press
+        self.w_collection_table.b_refresh.clicked.connect(
+            self.refresh_documents_in_table
         )
-        self.table.customContextMenuRequested.connect(self.foo)
 
-    def foo(self, pos):
-        menu = QMenu()
+    @property
+    def current_collection_name(self) -> Optional[str]:
+        idx = self.tree.currentColumn()
+        item = self.tree.currentItem()
+        if not item:
+            return None
+        return item.text(idx)
+
+    def create_table_context_menu(self, pos):
         db_ids = set(
             item.db_id for item in self.table.selectedItems() if item is not None
         )
-        menu.addAction(f"Remove {len(db_ids)} document(s)")
+
+        menu = QMenu()
+        menu.addAction(
+            f"Remove {len(db_ids)} document(s)",
+            partial(self.delete_documents, db_ids),
+        )
         menu.exec(self.table.viewport().mapToGlobal(pos))
-        # if action and action.text() == TableAction.REMOVE_DOC:
-        #     print(self.table.selectedItems())
+
+    def delete_documents(self, doc_ids: List[str]):
+        coll = self.current_collection_name
+        for doc_id in doc_ids:
+            self.db.document(f"{coll}/{doc_id}").delete()
+        self.refresh_documents_in_table(coll)
 
     @Slot()
     def refresh_collections_list(self):
+        self.tree.clear()
+
         for col in self.db.collections():
             col: CollectionReference
-            item = QTreeWidgetItem(self.w_collections_tree.w_tree)
+            item = QTreeWidgetItem(self.tree)
             item.setText(0, col.id)
-            self.w_collections_tree.w_tree.addTopLevelItem(item)
+            self.tree.addTopLevelItem(item)
 
     @Slot()
     def _on_collection_double_clicked(self, item: QTreeWidgetItem, *_):
@@ -68,7 +91,15 @@ class DatabaseView(DatabaseViewWidget):
         self.refresh_documents_in_table(col_name)
         self.w_stack.setCurrentWidget(self.page_table)
 
-    def refresh_documents_in_table(self, col_name: str):
+    def _on_table_item_key_press(self, event: QKeyEvent):
+        if event.text() == " ":
+            data = self.table.currentItem().content
+            dialog = DocumentValueDialog(data)
+            dialog.exec()
+
+    def refresh_documents_in_table(self, col_name: Optional[str] = None):
+        if not col_name:
+            col_name = self.current_collection_name
         docs = []
         headers = set()
         for doc in self.db.collection(col_name).stream():
@@ -92,8 +123,5 @@ class DatabaseView(DatabaseViewWidget):
         self.table.setHorizontalHeaderLabels(headers)
         for i, doc in enumerate(docs):
             for k, v in doc.items():
-                # item = QTableWidgetItem(str(v))
-                item = TableItem(doc.get("id", doc.get("uuid", None)), str(v))
-                # item.db_id = doc.get("uuid", doc.get("id", None))
+                item = TableItem(doc.get("id", doc.get("uuid", None)), v)
                 self.table.setItem(i, header_pos[k], item)
-                # self.row_to_db_id[i] = doc.get("uuid", doc.get("id", None))
---
+++ app/widgets/dialogs/document_value.py
@@ -0,0 +1,34 @@
+import json
+
+from PySide6.QtGui import QKeyEvent
+from PySide6.QtWidgets import QDialog
+
+from app.utils import apply_theme
+from app.widgets.auto.dialog_document_value import Ui_Dialog
+
+
+class DocumentValueDialog(QDialog, Ui_Dialog):
+    def __init__(self, value, *args):
+        super().__init__(*args)
+
+        self.setupUi(self)
+        apply_theme(self)
+
+        self._value = value
+
+        self.lbl_formatter.setText(self.pretty_value)
+        self.te_value.setText(str(self._value))
+        self.lbl_formatter.setFocus()
+
+    @property
+    def pretty_value(self) -> str:
+        if isinstance(self._value, (list, dict)):
+            return json.dumps(self._value, indent=2)
+        # if hasattr(self._value, "strftime"):
+        #     return self._value.strftime("%m-%d-%Y - %H:%M:%S")
+        return str(self._value)
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        if event.text() == " ":
+            self.accept()
+        super().keyPressEvent(event)
